89a9a733da2088faa9a99fd920a5920b2b0648d8
Andrea <andrea.fronzi@studio.unibo.it>
2024-11-08 18:06:32 +0100

    add the resolution of exercise which concernes Month with enumeration

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -9,13 +9,78 @@ import java.util.Objects;
  */
 public final class MonthSorterNested implements MonthSorter {
 
+    private enum Month{
+        JANUARY(31),
+        FEBRUARY(28),
+        MARCH(31),
+        APRIL(30),
+        MAY(31),
+        JUNE(30),
+        JULY(31),
+        AUGUST(31),
+        SEPTEMBER(30),
+        OCTOBER(31),
+        NOVEMBER(30),
+        DECEMBER(31);
+
+        private final int days;
+
+        Month(final int days){
+            this.days = days;
+        }
+
+        public static Month fromString(final String monthName){
+            Objects.requireNonNull(monthName);
+            try{
+                return valueOf(monthName);
+            }catch(IllegalArgumentException e){
+                Month match = null;
+                for(final Month month: values()){
+                    if(month.toString().toLowerCase().startsWith(monthName.toString().toLowerCase())){
+                        if(match != null){
+                            throw new IllegalArgumentException(monthName + " is ambiguous: both " + match + " and " + month + " would be valid matches", e);
+                        }
+                        match = month;
+                    }
+                }
+                if(match == null){
+                    final String msg = "your insertion " +monthName+ "does not corrispond with any month";
+                    throw new IllegalArgumentException(msg);
+                }
+                return match;
+            }
+        }
+
+    }
+
     @Override
     public Comparator<String> sortByDays() {
-        return null;
+        return new SortByDate();
     }
 
     @Override
     public Comparator<String> sortByOrder() {
-        return null;
+        return new SortByMonthOrder();
+    }
+
+    private class SortByMonthOrder implements Comparator<String>{
+
+        @Override
+        public int compare(String arg0, String arg1) {
+            final Month month1 = Month.fromString(arg0);
+            final Month month2 = Month.fromString(arg1);
+            return month1.compareTo(month2);
+        }
+        
+    }
+
+    private final static class SortByDate implements Comparator<String>{
+        @Override
+        public int compare(String arg0, String arg1) {
+            final Month month1 = Month.fromString(arg0);
+            final Month month2 = Month.fromString(arg1);
+            return Integer.compare(month1.days, month2.days);
+        }
+        
     }
 }
